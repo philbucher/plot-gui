f80da412cb235bc94198e759213203d96f9d783d
Philipp Bucher <philipp.bucher@tum.de>
2024-04-21 22:04:26 +0200

    WIP improve data module

--- src/data.rs
+++ src/data.rs
@@ -12,7 +12,8 @@ use std::collections::HashMap;
 {
     name: "DEBUT"
     path: "proc.0" // or "global"
-    memory: 123.5 // VM_Curr => does not exist for "global" folder
+    memory_current: 123.5 // VM_Curr => does not exist for "global" folder
+    memory_peak: 123.5 // VM_Curr => does not exist for "global" folder
     files: {
         "glob.1" : 1236 // B
         "fort.20" : 547 // B
@@ -26,11 +27,12 @@ use std::collections::HashMap;
 // this is parsed from the logs
 struct DataPoint
 {
-    memory: Option<f64>,
+    memory_current: Option<f64>,
+    memory_peak: Option<f64>,
     files: HashMap<String, f64>,
     counter: usize // reordered consecutive (must happen during reading!
     date_time: ...
-    rank: Option<i32>
+    rank: Option<u32>
 }
 
 #[derive(PartialEq, Clone, Copy)]
@@ -72,4 +74,31 @@ impl DataSeries {
 
 
     pub fn get_data() -> Vec<DataPoint> {}
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_deserialize_data_point() {
+        let data = r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+    }
+
+    #[test]
+    fn test_bad_add() {
+        // This assert would fire and test will fail.
+        // Please note, that private functions can be tested too!
+        assert_eq!(bad_add(1, 2), 3);
+    }
 }
